64953f918f73ffbc364767f25347df361b11eb77
DanielhCarranza <ingbiodanielh@gmail.com>
2020-07-14 05:18:39 +0000

    hello

---
+++ TF_SimilarityAbstractSearch/similarity_abstract_search/datasets/datacleaning.py
@@ -0,0 +1,87 @@
+import os
+import json
+import pandas as pd
+import numpy as np
+import h5py
+from pathlib import Path 
+from langid.langid import LanguageIdentifier, model
+from similarity_abstract_search.utils import get_paper_set, get_raw_json_data, save_dict2json
+
+Path.ls = lambda x: list(x.iterdir())
+
+class TextDataCleaning:
+  def __init__(self, data_files:list, paper_set_file=None):
+    self.data_files = data_files
+    if paper_set_file is not None:
+      self.paper_set = get_paper_set(paper_set_file)
+    identifier = LanguageIdentifier.from_modelstring(model, norm_probs=True)
+    self.lang = lambda s : identifier.classify(str(s)) 
+
+  def pruning(self, df)->pd.DataFrame:
+    "Pruning: Reduce text without losing meaning"
+    return df[df["totalCitation"].transform(lambda x: bool(set(x)&self.paper_set))]
+  
+  def remove_nonenglish(self, df, journalSet=set())->pd.DataFrame:
+    jSet = journalSet|set(df['journalName'])
+    vSet = journalSet|set(df['venue'])
+    engJournals=[i for i in jSet if str(self.lang(i)).find('en') != -1 and self.lang(i)[1]>0.5]  
+    engVenues = [i for i in vSet if str(self.lang(i)).find('en') != -1 and self.lang(i)[1]>0.5]  
+    df = df[df['journalName'].isin(engJournals) | df['venue'].isin(engVenues)]
+    df.drop(['journalName','venue'], axis=1, inplace=True)
+    return df
+
+
+  # def embProcessing(self, df):
+  #   df['citeEmbeddingsID']=df.totalCitation.map(lambda row: [self.paperID2EmbeddingID[i] 
+  #                                     for i in row if self.paperID2EmbeddingID.get(i)])
+  #   df['EmbeddingID'] = df['id'].map(self.paperID2EmbeddingID) 
+  #   df = df.loc[:,['EmbeddingID', 'paperAbstract', 'title', 'citeEmbeddingsID']]
+  #   return df
+  
+
+  @staticmethod
+  def saveEmbedIDs(df, filename):
+    embIDs = df.loc[:,['EmbeddingID', 'citeEmbeddingsID']]
+    embIDs['citeEmbeddingsID'] = embIDs['citeEmbeddingsID'].apply(lambda x: np.array(x)) 
+    
+    h5f = h5py.File(f'{filename}.h5', 'w')
+    dt = h5py.special_dtype(vlen=np.dtype('int32'))
+    h5f.create_dataset('cites', data=embIDs.values[:,1], dtype=dt, compression='gzip', compression_opts=9, chunks=True, maxshape=(None,) )
+    h5f.create_dataset('paper', data=embIDs.values[:,0].astype(np.int32), compression='gzip', compression_opts=9, chunks=True, maxshape=(None,) )
+    h5f.close()
+  
+
+  def pruning_and_cleaning(self, data_path):
+    paper_set = set()
+    for fn in self.data_files:
+      df = get_raw_json_data(fn)
+      print(fn)
+      df = self.pruning(df)
+      df = self.remove_nonenglish(df)
+      paper_set.update(set(df.id.values))
+      df.to_json(data_path/f'pruned{fn.stem[-3:]}.json.gzip', compression='gzip')
+      # os.remove(str(fn))
+
+    self.paperID2EmbeddingID = {id: idx for idx, id in enumerate(paper_set)}
+    save_dict2json(str(data_path.parent/'paperID2emb' ), self.paperID2EmbeddingID)
+  
+    # df = pd.concat([pd.read_json(data_path/'pruned{fn.stem[-3:]}.json.gzip', compression='gzip') 
+    #                 for fn in self.data_files])
+    # df.to_json(data_path/'pruned_and_clean.json.gzip', compression='gzip')
+
+  def concat_files(self, data_path):
+    df = pd.concat([pd.read_json(data_path/'pruned{fn.stem[-3:]}.json.gzip', compression='gzip') 
+                    for fn in self.data_files])
+    df.to_json(data_path/'pruned_and_clean.json.gzip', compression='gzip')
+    
+
+def main():
+  INNER_PATH = Path(__file__).resolve().parents[3]/'Data/processed/SemanticScholarData'
+  PAPER_SET  = INNER_PATH.parent/'paper_set.txt'
+  data_files = INNER_PATH.ls()
+  data_cleaning = TextDataCleaning(data_files[:2], PAPER_SET)
+  data_cleaning.pruning_and_cleaning(INNER_PATH)
+
+
+if __name__ == "__main__":
+     main()
---
+++ TF_SimilarityAbstractSearch/similarity_abstract_search/utils.py
@@ -0,0 +1,40 @@
+import gc 
+import os
+import re
+import json 
+import hashlib
+import numpy as np
+import pandas as pd 
+from tqdm import tqdm
+from concurrent.futures import as_completed, ThreadPoolExecutor
+from pathlib import Path
+from typing import Union, Tuple, List
+from urllib.request import urlopen, urlretrieve
+
+
+def compute_sha256(filename: Union[Path, str]):
+    """Return SHA256 checksum of a file."""
+    with open(filename, "rb") as f:
+        return hashlib.sha256(f.read()).hexdigest()
+
+### Processing 
+open_raw_data_json = lambda j : json.loads(json.loads(j))
+
+def get_raw_json_data(filename: Union[Path, str]):
+  f = open(filename)
+  jsonlist = f.readlines()
+  df = pd.DataFrame(map(open_raw_data_json, jsonlist))
+  del jsonlist, f
+  gc.collect()
+  return df
+
+def get_paper_set(file: Union[Path, str])->set:
+  file=str(file)
+  if file.endswith('.txt'):
+    ps = set(open(file).read().replace('"',"").splitlines())
+  else: print('File not in the correct format')
+  return ps
+
+def save_dict2json(filename, dictObj):
+  with open(f'{filename}.json', 'w') as outfile:
+    json.dump(dictObj, outfile)
